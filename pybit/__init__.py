a6649534f130845e0504cf301103b431890fc699
Jerry Boonstra <verysolvedsoftware@gmail.com>
2021-12-28 13:53:47 -0800

    two more methods should throw Exception in error state, instead of just logging an error and returning nothing

--- pybit/__init__.py
+++ pybit/__init__.py
@@ -1573,7 +1573,7 @@ class HTTP:
                 auth=True
             )
         else:
-            self.logger.error('amount must be in string format')
+            raise Exception(f"amount must be in string format, got: '{kwargs.get('amount', None)}'")
 
     def create_subaccount_transfer(self, **kwargs):
         """
@@ -1594,7 +1594,7 @@ class HTTP:
                 auth=True
             )
         else:
-            self.logger.error('amount must be in string format')
+            raise Exception(f"amount must be in string format, got: '{kwargs.get('amount', None)}'")
 
     def query_transfer_list(self, **kwargs):
         """
