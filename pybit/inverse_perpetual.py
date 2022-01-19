944d506d8ff1eb96261645b790f1f9e52bf4ced4
dextertd <dexterdickinson@protonmail.com>
2022-01-19 15:53:33 +0000

    support symbol arg

--- pybit/inverse_perpetual.py
+++ pybit/inverse_perpetual.py
@@ -1,7 +1,8 @@
 from .websocket_stream import FuturesWebSocketManager
+from .websocket_stream import INVERSE_PERPETUAL
 
 
-ws_name = "Inverse Perp"
+ws_name = INVERSE_PERPETUAL
 WSS = "wss://{SUBDOMAIN}.{DOMAIN}.com/realtime"
 
 
@@ -11,10 +12,16 @@ class WebSocket(FuturesWebSocketManager):
         super().__init__(WSS, ws_name, test=test, domain=domain,
                          api_key=api_key, api_secret=api_secret)
 
-    def instrument_info_stream(self, callback):
-        topic = "instrument_info.100ms.BTCUSD"
-        self.subscribe(topic, callback)
+    def instrument_info_stream(self, callback, symbol):
+        """
+        https://bybit-exchange.github.io/docs/inverse/#t-websocketinstrumentinfo
+        """
+        topic = "instrument_info.100ms.{}"
+        self.subscribe(topic, callback, symbol)
 
     def position_stream(self, callback):
+        """
+        https://bybit-exchange.github.io/docs/inverse/#t-websocketposition
+        """
         topic = "position"
         self.subscribe(topic, callback)
